ab56ff5c7647684a0e31e44958893b811a841462
gnsksz <gnsk.sz@gmail.com>
2021-10-24 00:27:12 +0900

    added try catch to log error

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -15,34 +15,37 @@ const config_path = (process.env.NODE_ENV === 'production')
 
 log4js.configure(config_path);
 
-const c = getIrcConfig();
-if (c.nick == "your account id" || c.opt.password == "you can get password from 'https://osu.ppy.sh/p/irc'") {
-  logger.error("you must enter your account name and irc password in the config file. ");
-  logger.error("you can get the password from 'https://osu.ppy.sh/p/irc' ");
-  logger.error("Copy config/default.json to config/local.json, and enter your id and irc password.");
-  process.exit(1);
-}
-
-let client = new irc.Client(c.server, c.nick, c.opt);
-client.on("error", err => {
-  if (err.command == "err_passwdmismatch") {
-    logger.error('%s: %s', err.command, err.args.join(' '));
-    logger.error("check your account id and password.");
+try {
+  const c = getIrcConfig();
+  if (c.nick == "your account id" || c.opt.password == "you can get password from 'https://osu.ppy.sh/p/irc'") {
+    logger.error("you must enter your account name and irc password in the config file. ");
+    logger.error("you can get the password from 'https://osu.ppy.sh/p/irc' ");
+    logger.error("Copy config/default.json to config/local.json, and enter your id and irc password.");
     process.exit(1);
   }
-});
 
-logIrcEvent(client);
-logPrivateMessage(client);
+  let client = new irc.Client(c.server, c.nick, c.opt);
+  client.on("error", err => {
+    if (err.command == "err_passwdmismatch") {
+      logger.error('%s: %s', err.command, err.args.join(' '));
+      logger.error("check your account id and password.");
+      process.exit(1);
+    }
+  });
 
-if (process.argv.length > 2) {
-  const command = process.argv[2];
-  const oahr = new OahrHeadless(client);
-  const arg = process.argv.slice(3).join(" ");
-  oahr.start(command, arg);
-} else {
-  const oahr = new OahrCli(client);
-  oahr.start(null);
+  logIrcEvent(client);
+  logPrivateMessage(client);
+
+  if (process.argv.length > 2) {
+    const command = process.argv[2];
+    const oahr = new OahrHeadless(client);
+    const arg = process.argv.slice(3).join(" ");
+    oahr.start(command, arg);
+  } else {
+    const oahr = new OahrCli(client);
+    oahr.start(null);
+  }
+} catch (e: any) {
+  logger.error(e);
+  process.exit(1);
 }
-
-
