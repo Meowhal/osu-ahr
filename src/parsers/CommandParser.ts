96d1687d2054c48a1c02276dd32c98f76026d4ac
Xayanide <chris.balili.91@gmail.com>
2022-05-04 17:26:47 +0800

    refactor(parsers): add file extension on import paths

--- src/parsers/CommandParser.ts
+++ src/parsers/CommandParser.ts
@@ -1,4 +1,4 @@
-import { Teams } from '../Player';
+import { Teams } from '../Player.js';
 export namespace parser {
   export function ParseBanchoResponse(message: string): BanchoResponse {
     // １文字目を整数比較してifの評価回数を減らす
