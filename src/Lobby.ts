d51d4aaa65a1beb49397fbef1234249a444cced4
Xayanide <chris.balili.91@gmail.com>
2022-06-03 13:59:56 +0800

    refactor(src#Lobby): emphasize beatmap link and title with newlines

--- src/Lobby.ts
+++ src/Lobby.ts
@@ -527,7 +527,7 @@ export class Lobby {
           this.mapId = c.params[0];
           this.mapTitle = c.params[1];
           const changer = this.host ? `by ${c.type === BanchoResponseType.BeatmapChanged ? this.host.name : 'Bot'}` : '';
-          this.logger.info(`Beatmap has been changed ${changer}: https://osu.ppy.sh/b/${this.mapId} ${this.mapTitle}`);
+          this.logger.info(`Beatmap has been changed ${changer}:\nhttps://osu.ppy.sh/b/${this.mapId}\n${this.mapTitle}`);
         }
         break;
       case BanchoResponseType.Settings:
