537ee6eaa8adcd2cd801eb08e5e8bc97e14b8c40
Pyrozen <zeropyrozen@gmail.com>
2022-07-11 11:20:06 +0700

    Apply code from review
    Co-Authored-By: Xayanide <18362871+Xayanide@users.noreply.github.com>

--- dist/plugins/MatchStarter.js
+++ dist/plugins/MatchStarter.js
@@ -130,7 +130,7 @@ class MatchStarter extends LobbyPlugin_1.LobbyPlugin {
             if (count > 15) {
                 this.lobby.DeferMessage('Match starts in 10 seconds', 'mp_start 10 sec', (count - 10) * 1000, true);
             }
-            if (35 < count) {
+            if (count > 35) {
                 this.lobby.DeferMessage("Match starts in 30 seconds", "mp_start 30 sec", (count - 30) * 1000, true);
             }
         }
